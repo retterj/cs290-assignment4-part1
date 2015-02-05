fd7ee0c60de12ca50bcf3f8bd23953a913c3eee5
Jen Retter <jen.retter@gmail.com>
2015-02-04 21:28:25 -0600

    Added files and started project

---
+++ src/multtable.php
@@ -0,0 +1,20 @@
+<!--needs 4 parameters from GET request: 
+min-multiplicand
+max-multiplicand
+min-multiplier
+max-multiplier--!>
+
+<html>
+	<?php
+	<form action="what url here??" method="get">
+		<p>GET data</p>    <!--Get version; same code for both-->
+		<p>min-multiplicand: <input type="number" name="min-multiplicand"></p>
+		<p>max-multiplicand: <input type="number" name="max-multiplicand"></p>
+		<p>min-multiplier: <input type="number" name="min-multiplier"></p>
+		<p>max-multiplier: <input type="number" name="max-multipler"></p>
+		
+		
+		<input type="submit" value="Submit">	
+	</form>
+	?>
+</html>
