a7be98eb765dcbd9be21ec9192bf325b025d03b9
Jen Retter <jen.retter@gmail.com>
2015-02-10 15:26:23 -0600

    Updated multtable, started login

---
+++ src/login.php
@@ -0,0 +1,6 @@
+<?php
+	<form action=? method="post">
+		<p>Enter username: <input type="text" name="username"></p>
+		<input type="submit" value="Login">	
+	</form>
+?>
--- src/multtable.php
+++ src/multtable.php
@@ -1,24 +1,51 @@
-<!--needs 4 parameters from GET request: min-multiplicand--!>
-<!--max-multiplicand, min-multiplier, max-multiplier--!>
+<!--needs 4 parameters from GET request: min-multiplicand-->
+<!--max-multiplicand, min-multiplier, max-multiplier-->
 <!DOCTYPE html>
+<body>
 	<?php
-		$_GET["min-multiplicand"];
-		$_GET["max-multiplicand"];
-		$_GET["min-multiplier"];
-		$_GET["max-multiplier"];
-	?>
-		if (<?php $_GET['min-multiplicand']>$_GET['max-multiplicand']?>){
+		$min_multiplicand = $_GET["min-multiplicand"];
+		$max_multiplicand = $_GET["max-multiplicand"];
+		$min_multiplier = $_GET["min-multiplier"];
+		$max_multiplier = $_GET["max-multiplier"];
+	
+		if ($_GET['min-multiplicand']>$_GET['max-multiplicand']){
 			echo "Minimum multiplicand is larger than maximum multiplicand. <br>";
 		}
 
-		if (<?php $_GET['min-multiplier']>$_GET['max-multiplier']?>){
+		if ($_GET['min-multiplier']>$_GET['max-multiplier']){
 			echo "Minimum multiplier is larger than maximum multiplier. <br>";
 		}
-
 	
+		if(is_null($min_multiplicand)){
+			echo "Minimum multiplicand is null. <br";
+		}
+
+		if(is_null($max_multiplicand)){
+			echo "Maximum multiplicand is null. <br";
+		}
+		
+		if(is_null($min_multiplier)){
+			echo "Minimum multiplier is null. <br";
+		}
+		
+		if(is_null($max_multiplier)){
+			echo "Maximum multiplier is null. <br";
+		}
+
+	?>
 	<table>
 		<tbody>
-			<tr>
-				<td>$_GET['min-multiplier']</td>
-			</tr>
+			<?php
+		//--Need to display all multiplicands and multiplers, too
+			for($i=$min_multiplicand; $i=$max_multiplicand; $i++){
+   				echo("<tr>");
+   					for($j=$min_multiplier; $j=$max_multiplier; $j++){
+   						echo ("<td> $i*$j </td>");
+   					}
+  				echo("</tr>");
+				} ?>
+			</tbody>
+	</table>
+	
+</body>
 </html>
